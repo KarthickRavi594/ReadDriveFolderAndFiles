1c4250127a8c686c9113f9ac984d6d828d47acfa
Karthick <32698132+KarthickRavi594@users.noreply.github.com>
2019-12-02 17:39:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// var express = require('express');
+// var bodyParser = require("body-parser");
+// const xslx = require('xlsx')
+// const workBook = xslx.readFile('UnitInventories.xlsx')
+// const sheet_name_list = workBook.SheetNames;
+
+// var app = express();
+
+const port = process.env.PORT || 3456;
+
+// app.listen(port, err => {
+//     if (err) console.log(err);
+//     else {
+//         console.log("Running in ", port);
+//     }
+// });
+
+// console.log(xslx.utils.sheet_to_json(workBook.Sheets[sheet_name_list[0]]));
+
+// app.use(bodyParser.json());
+
+// const drivelist = require('drivelist');
+ 
+// const drives = drivelist.list();
+// console.log(drives);
+
+// app.get('/',(req,res)=>{
+
+// })
